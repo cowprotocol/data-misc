007e396437edb046e5647a35045430c145f06d25
Ben Smith <bh2smith@gmail.com>
2022-06-17 15:44:25 +0200

    cleanup

--- src/missing_tokens.py
+++ src/missing_tokens.py
@@ -50,11 +50,10 @@ if __name__ == "__main__":
         Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{os.environ['INFURA_KEY']}")
     )
     dune_conn = DuneAPI.new_from_environment()
-
+    print("Getting missing tokens from: https://dune.com/queries/236085")
     missing_tokens = fetch_missing_tokens(dune_conn)
-    print(f"Found {len(missing_tokens)} missing tokens.")
-    # TODO batch these calls.
-    token_details = [TokenDetails(address=t) for t in missing_tokens]
-    for token in token_details:
-        print(token)
+    print(f"Found {len(missing_tokens)} missing tokens, fetching metadata...")
+    # TODO batch the eth_calls used to construct the token contracts.
+    token_details = [str(TokenDetails(address=t)) for t in missing_tokens]
+    print("\n".join(token_details))
 
