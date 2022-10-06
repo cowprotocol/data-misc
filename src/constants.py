05848251e97b0e3192211600db1976d658e00602
Ben Smith <bh2smith@gmail.com>
2022-10-06 14:12:23 +0200

    reduce diff by leaving erc20 abi alone

--- src/constants.py
+++ src/constants.py
@@ -1,32 +1,32 @@
 ERC20_ABI = [
-    {
-        "constant": True,
-        "inputs": [],
-        "name": "decimals",
-        "outputs": [
-            {
-                "name": "",
-                "type": "uint8"
-            }
-        ],
-        "payable": False,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": True,
-        "inputs": [],
-        "name": "symbol",
-        "outputs": [
-            {
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "payable": False,
-        "stateMutability": "view",
-        "type": "function"
-    }
+  {
+    "constant": True,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": True,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": False,
+    "stateMutability": "view",
+    "type": "function"
+  }
 ]
 
 PUBLIC_RESOLVER_ABI = [
