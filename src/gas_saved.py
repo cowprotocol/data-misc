5808cc45eaffc7f90b2f034b72d6a09fd85ada6f
Ben Smith <bh2smith@gmail.com>
2022-10-19 10:19:29 +0200

    lint gas_saved.py

--- src/gas_saved.py
+++ src/gas_saved.py
@@ -14,9 +14,12 @@ from src.db.pg_client import pg_engine
     help="The transaction hash of the batch you want to see the % gas saved.",
     type=str,
 )
-def main(batch_tx_hash):
+def main(batch_tx_hash: str) -> int:
+    """
+    Fetches gas saved for a given transaction hash.
+    """
     db_engine = pg_engine()
-    GAS_QUOTES_QUERY = f"""
+    gas_quotes_query = f"""
     SELECT orders.id::bytea, order_quotes.gas_amount, order_quotes.gas_price
     FROM
         solver_competitions,
@@ -25,8 +28,9 @@ def main(batch_tx_hash):
     WHERE
       solver_competitions.tx_hash = '\\{batch_tx_hash[1:]}'
     """
-    df_quotes = pd.read_sql(GAS_QUOTES_QUERY, db_engine)
+    df_quotes = pd.read_sql(gas_quotes_query, db_engine)
     # subtract settlement_overhead from price estimation
+    # pylint:disable=line-too-long
     # Ref: https://github.com/cowprotocol/services/blob/fd5f7cf47a6afdff89b310b60b869dfc577ac7a7/crates/shared/src/price_estimation/gas.rs#L37
     df_quotes["gas_amount"] = df_quotes["gas_amount"].apply(lambda x: x - 106391)
     load_dotenv()
@@ -38,12 +42,16 @@ def main(batch_tx_hash):
         f"Trades executed individually would have cost {df_quotes['gas_amount'].sum():.0f} gas."
     )
     print(f"Batch only used {tx.gasUsed} gas.")
+    sum_gas = df_quotes["gas_amount"].sum()
+    gas_saved = sum_gas - tx.gasUsed
+    decrease = (gas_saved / sum_gas) * 100
     print(
-        f"This resulted in {df_quotes['gas_amount'].sum() - tx.gasUsed:.0f} absolute gas saved and "
-        f"{((df_quotes['gas_amount'].sum() - tx.gasUsed) / df_quotes['gas_amount'].sum()) * 100:.2f}% decrease of gas."
+        f"This resulted in {gas_saved:.0f} absolute gas saved and {decrease:.2f}% decrease of gas."
     )
     return 0
 
 
 if __name__ == "__main__":
+    # TODO - figure out how to get this linted with the click decorator!
+    # pylint:disable=no-value-for-parameter
     main()
